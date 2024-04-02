686deec4ce575cabafb7929e6303edfee510df54
Xiao <101236069+Mrxia@users.noreply.github.com>
2024-04-03 00:15:15 +0800

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -26,7 +26,7 @@ sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_genera
 # svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
 svn co https://github.com/EOYOHOO/UA2F package/UA2F
 svn co https://github.com/EOYOHOO/rkp-ipid package/rkp-ipid
-svn co https://github.com/linkease/istore package/istore
+
 
 
 # 修改系统名
