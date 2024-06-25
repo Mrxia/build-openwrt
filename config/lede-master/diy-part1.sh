ab7407e289e0a47ee49edd0659ff3380c043e699
Xiao <101236069+Mrxia@users.noreply.github.com>
2024-06-25 16:15:55 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -8,7 +8,7 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '1i src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+#sed -i '1i src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
 
 # other
