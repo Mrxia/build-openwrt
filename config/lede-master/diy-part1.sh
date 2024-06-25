e405530100a3f805c16dd0b1e5983b91cad9cf8f
Xiao <101236069+Mrxia@users.noreply.github.com>
2024-06-25 16:19:08 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -10,6 +10,8 @@
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '1i src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
+git clone https://github.com/Zxilly/UA2F.git package/UA2F
+git clone https://github.com/CHN-beta/rkp-ipid.git package/rkp-ipid
 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
